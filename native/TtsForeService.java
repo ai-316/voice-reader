fb0dc656b9d6306e8592104701b7c05ea5104963
ai-316 <316malsum@gmail.com>
2026-09-03 16:27:40 +0900

    Add import for android.os.Bundle in TtsForeService

--- native/TtsForeService.java
+++ native/TtsForeService.java
@@ -8,6 +8,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
